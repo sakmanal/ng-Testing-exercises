4ecb811a6796e45bfc4ab24771f0e9b5796a7613
Sakis Manal <manowarlik@gmail.com>
2020-12-27 00:06:03 +0200

    add AuthGuard Service tests

--- src/app/core/guards/auth-gurd.service.ts
+++ src/app/core/guards/auth-gurd.service.ts
@@ -9,7 +9,7 @@ export class AuthGurdService implements CanActivate{
 
   constructor(private router: Router, private authService: AuthService) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(route?: ActivatedRouteSnapshot, state?: RouterStateSnapshot): boolean {
 
     if (this.authService.isLoggedIn) {
         // logged in so return true
